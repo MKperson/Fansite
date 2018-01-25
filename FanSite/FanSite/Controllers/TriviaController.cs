f9f41f4145d8f7a9e91008a5bac9dac2436f3d3d
Mike Kvederis <m_kvederis@msn.com>
2018-01-25 13:31:19 -0800

    Trivia quiz up and running

--- FanSite/FanSite/Controllers/TriviaController.cs
+++ FanSite/FanSite/Controllers/TriviaController.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FanSite.Models;
 
 namespace FanSite.Controllers
 {
     public class TriviaController : Controller
     {
-        public IActionResult Index()
+        /*public IActionResult Index()
         {
-            return View("TriviaQuiz");
+            TriviaLogic logic = new TriviaLogic();
+            return View("TriviaQuiz",logic);
+        }*/
+        //[HttpPost]
+        public IActionResult Index(string Q1,string Q2,string Q3,string Q4,string Q5)
+        {
+            TriviaLogic logic = new TriviaLogic(Q1, Q2, Q3, Q4, Q5);
+            return View("TriviaQuiz",logic);
         }
         //TODO Multipule Choice
-        //Form With Questions and Radio buttons
-        //5-10 questions
+        //Form With Questions and Radio buttons DONE
+        //5-10 questions DONE
         //display right or wrong for each answer
         //total right
     }
---
+++ FanSite/FanSite/Models/TriviaLogic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanSite.Models
+{
+    public class TriviaLogic
+    {
+        private string q1, q2, q3, q4, q5;
+        public TriviaLogic()
+        {
+            q1 = null;
+            q2 = null;
+            q3 = null;
+            q4 = null;
+            q4 = null;
+        }
+        public TriviaLogic(string Q1, string Q2, string Q3, string Q4, string Q5)
+        {
+            q1 = Q1;
+            q2 = Q2;
+            q3 = Q3;
+            q4 = Q4;
+            q5 = Q5;
+
+        }
+        public int Test()
+        {
+            int num = 0;
+            if (q1 == "a")
+            {
+                num++;
+            }
+            if (q2 == "c")
+            {
+                num++;
+            }
+            if (q3 == "a")
+            {
+                num++;
+            }
+            if (q4 == "d")
+            {
+                num++;
+            }
+            if (q5 == "a")
+            {
+                num++;
+            }
+
+            return num;
+        }
+
+    }
+}
+
