cc3ef37b2b4732329522c304ce6fb2726a7524f1
Mike Kvederis <m_kvederis@msn.com>
2018-02-08 14:00:21 -0800

    added mor pictures

---
+++ FanSite/FanSite/Models/PictureClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FanSite.Models
+{
+    public class PictureClass
+    {
+        
+        
+        public List<string> FileNames { get; set; }
+
+
+        public PictureClass()
+        {
+            DirectoryInfo files = new DirectoryInfo(@".\wwwroot\images\");
+            FileInfo[] fileinfos = null;
+            try
+            {
+                fileinfos = files.GetFiles();
+            }
+            catch(Exception e)
+            {
+                //NOP
+            }
+
+            FileNames = files.GetFiles().Select(x => x.Name).ToList();
+            
+            
+        }
+    }
+}
