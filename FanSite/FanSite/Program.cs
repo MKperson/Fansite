f2b34c3ca0c2c7231e56a79e189e5ee330ce6a76
Mike Kvederis <m_kvederis@msn.com>
2018-02-13 11:34:09 -0800

    Added database and all stories link

--- FanSite/FanSite/Program.cs
+++ FanSite/FanSite/Program.cs
@@ -7,19 +7,45 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using FanSite.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FanSite
 {
     public class Program
     {
-        public static void Main(string[] args)
+        /*public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+        }*/
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
+
+
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseDefaultServiceProvider(options =>
+                    options.ValidateScopes = false)
                 .Build();
     }
 }
