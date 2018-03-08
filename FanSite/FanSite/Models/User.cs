52c8f47da96f13915402312f828b555dc5dd744d
Mike Kvederis <m_kvederis@msn.com>
2018-03-08 12:25:47 -0800

    took out user id

--- FanSite/FanSite/Models/User.cs
+++ FanSite/FanSite/Models/User.cs
@@ -4,7 +4,7 @@ namespace FanSite.Models
 {
     public class User : IdentityUser
     {
-        public int UserId { get; set; }
+        //public int UserId { get; set; }
         public string FirstName{get;set;}
         public string LastName { get; set; }
 
